08c62fa8cf115ea8fc8b8d5ce99f7ad82fc5ab3e
akk143 <163145180+akk143@users.noreply.github.com>
2026-09-17 20:24:18 +0700

    feat: add PWA support

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,41 @@
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
+import { VitePWA } from 'vite-plugin-pwa'
+
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
+
+    VitePWA({
+      registerType: 'autoUpdate',
+
+      manifest: {
+        name: 'PocketTrack',
+        short_name: 'PocketTrack',
+        description: 'Personal revenue and expense tracker',
+        theme_color: '#ffffff',
+        background_color: '#ffffff',
+        display: 'standalone',
+        start_url: '/',
+        scope: '/',
+        icons: [
+          {
+            src: '/pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: '/pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+
+    })
   ]
 })
